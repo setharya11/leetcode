397cc2e946e08f490a3dd5d591d87d4f191f1b99
arya seth <setharya11@gmail.com>
2025-09-08 14:38:29 +0530

    Moved solutions into Easy folder

---
+++ easy/1317.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for(int i=0;i<n;i++){
+            int j=n-i;
+           if( to_string(i).find('0')==string::npos && to_string(j).find('0')==string::npos)
+           return {i,j};
+        }
+        return {};
+    }
+};
