39c3c1f18ea80523bfa8a921b0e0022bcb7054e4
arya seth <setharya11@gmail.com>
2025-09-12 14:43:36 +0530

    solution of 3227

---
+++ 3227.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int cnt{};
+        for(auto i:s)   if(i=='a' || i=='e' || i=='i' ||i =='o' || i=='u')cnt++ ;
+        return (cnt>0)? 1: 0;
+    }
+};
